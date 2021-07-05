69e1800c73b9b5877b7bd331dfe4c13f89fd2e37
Abhishek Shukla <as3727904@gmail.com>
2021-07-06 00:51:24 +0530

    2nd

--- src/components/Homeflex.js
+++ src/components/Homeflex.js
@@ -1,30 +1,18 @@
 import React from 'react'
-
+import './Homeflex.css'
 function Homeflex() {
     return (
         <div>      
-           
-  <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg" className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src="https://m.media-amazon.com/images/I/61f6itSlTML._SX1500_.jpg" className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src="https://m.media-amazon.com/images/I/61WvNRX-ECL._SX1500_.jpg" className="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </a>
-</div>
+
+          <div className="flexImage">
+            <img className="innerImage" src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg"  />
+            <img className="innerImage" src="https://m.media-amazon.com/images/I/61f6itSlTML._SX1500_.jpg" alt="..."/>
+            <img src="https://m.media-amazon.com/images/I/61HinPmsWSL._SX1500_.jpg"className="innerImage"/>
+
+          </div>
+             
+          <img src="amazon.jpg" alt="" className="footer" />
+
         </div>
     )
 }
